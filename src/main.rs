e862e992a86316f44b11a065c1c8212335fbe134
Danilo Bargen <mail@dbrgn.ch>
2021-01-18 00:41:20 +0100

    Argument parsing
    The API is compatible to raw-thumbnailer.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
-use std::{convert::TryInto, ffi::CString, io::Cursor, path::Path};
+use std::{
+    convert::TryInto,
+    ffi::{CString, OsStr},
+    io::Cursor,
+    path::{Path, PathBuf},
+};
+
+// Note: Windows is not yet supported.
+// See https://internals.rust-lang.org/t/pathbuf-to-cstring/12560 for details.
+use std::os::unix::ffi::OsStrExt;
 
 use anyhow::{bail, Result};
+use clap::Clap;
 use image::{io::Reader as ImageReader, DynamicImage, ImageFormat};
 
 mod libopenraw;
 
 use libopenraw::ffi;
 use libopenraw::ffi::ORThumbnailRef;
+
+#[derive(Clap)]
+#[clap(about, version)]
+struct Opts {
+    /// Input file
+    #[clap(short = 'i')]
+    input_path: PathBuf,
+
+    /// Output file (PNG)
+    #[clap(short = 'o')]
+    output_path: PathBuf,
+
+    /// Thumbnail size
+    #[clap(short = 's', default_value = "128")]
+    thumbnail_size: u32,
+}
 
 /// Convert a libopenraw thumbnail data type to an image-rs `ImageFormat`.
 fn data_type_to_image_format(data_type: ffi::or_data_type::Type) -> Result<ImageFormat> {
@@ -50,13 +76,15 @@ fn save_thumbnail(img: DynamicImage, output_path: &Path, thumbnail_size: u32) ->
 }
 
 fn main() -> anyhow::Result<()> {
-    let input_file = CString::new("DSCF0671.RAF").unwrap();
-    let thumbnail_size = 128;
+    // Parse command line arguments
+    let opts: Opts = Opts::parse();
 
     // Create a new rawfile
+    let input_path_os_str: &OsStr = &opts.input_path.as_ref();
+    let input_path = CString::new(input_path_os_str.as_bytes()).unwrap();
     let rawfile = unsafe {
         ffi::or_rawfile_new(
-            input_file.as_ptr(),
+            input_path.as_ptr(),
             ffi::or_rawfile_type::OR_RAWFILE_TYPE_UNKNOWN,
         )
     };
@@ -66,7 +94,7 @@ fn main() -> anyhow::Result<()> {
 
     // Get thumbnail
     let thumbnail = unsafe { ffi::or_thumbnail_new() };
-    let err = unsafe { ffi::or_rawfile_get_thumbnail(rawfile, thumbnail_size, thumbnail) };
+    let err = unsafe { ffi::or_rawfile_get_thumbnail(rawfile, opts.thumbnail_size, thumbnail) };
     if err != ffi::or_error::OR_ERROR_NONE {
         // TODO: Parse error code into enum
         bail!("Extracting thumbnail data failed with error code {}", err);
@@ -91,7 +119,7 @@ fn main() -> anyhow::Result<()> {
     }
 
     // Write output file
-    save_thumbnail(img, &Path::new("thumbnail.png"), thumbnail_size)?;
+    save_thumbnail(img, &opts.output_path, opts.thumbnail_size)?;
 
     // TODO exif rotate
 
